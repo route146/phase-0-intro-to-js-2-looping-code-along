462438bf60ae73e7ea35ed690b340dc3daf9ddb2
Micha <mlcheatham92@gmail.com>
2022-11-30 13:13:49 -0500

    commit tree b44aab3d63d93a25d5943233fae3abd505e321bd

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let cardList = [];
+    for (let i= 0; i < names.length; i++) {
+        cardList.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return cardList;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+        debugger;
+    }
+}
+
+countDown(10);
